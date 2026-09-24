f420a4061f56541302bb3609e0a4c83512420eeb
m4uz <m4uz@proton.me>
2026-09-24 13:51:38 +0200

    feat: name and unitName properties in Subject

--- lib/models/subject.g.dart
+++ lib/models/subject.g.dart
@@ -8,10 +8,14 @@ part of 'subject.dart';
 
 Subject _$SubjectFromJson(Map<String, dynamic> json) => Subject(
   uri: json['uri'] as String,
+  name: json['name'] as String? ?? '',
+  unitName: json['unitName'] as String? ?? '',
   modificationTime: DateTime.parse(json['modificationTime'] as String),
 );
 
 Map<String, dynamic> _$SubjectToJson(Subject instance) => <String, dynamic>{
   'uri': instance.uri,
+  'name': instance.name,
+  'unitName': instance.unitName,
   'modificationTime': instance.modificationTime.toIso8601String(),
 };
